b95def33ac0e37629afa9d33396aede376a57b4f
root <root@localhost.localdomain>
2019-01-27 06:27:59 +0500

    Add Main file

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+<<<<<<< HEAD
+func sendResponse(w http.ResponseWriter, r *http.Request) {
+  keys, ok := r.URL.Query()["a"]
+  if !ok || len(keys[0]) < 1 {
+      log.Println("Url Param 'a' is missing")
+      return
+  }
+
+  a, err := strconv.Atoi(keys[0])
+
+  if err != nil {
+    return
+  }
+
+  keys, ok = r.URL.Query()["b"]
+  if !ok || len(keys[0]) < 1 {
+      log.Println("Url Param 'b' is missing")
+      return
+  }
+  b, err := strconv.Atoi(keys[0])
+  if err != nil {
+    log.Println("Bad number!")
+    return
+  }
+
+  fmt.Fprintf(w, "%d + %d = %d", a, b, sum(a,b))
+}
+
+func main() {
+    http.HandleFunc("/", sendResponse)
+    log.Fatal(http.ListenAndServe(":7000", nil))
+}
+
+func sum (a, b int) int {
+<<<<<<< HEAD
+  t := b + a
+  return t
+=======
+  return a + b
+>>>>>>> 0b928bae1cdcf83660a30a9705eebb1fe163e8b3
+=======
+func handler(w http.ResponseWriter, r *http.Request) {
+	keys, ok := r.URL.Query()["a"]
+	if !ok || len(keys[0]) < 1 {
+		log.Println("Url Param 'a' is missing")
+		return
+	}
+
+	a, err := strconv.Atoi(keys[0])
+
+	if err != nil {
+		return
+	}
+
+	keys, ok = r.URL.Query()["b"]
+	if !ok || len(keys[0]) < 1 {
+		log.Println("Url Param 'b' is missing")
+		return
+	}
+	b, err := strconv.Atoi(keys[0])
+	if err != nil {
+		log.Println("Bad number!")
+		return
+	}
+
+	fmt.Fprintf(w, "%d + %d = %d", a, b, sum(a, b))
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":7000", nil))
+}
+
+func sum(a, b int) int {
+	t := a + b
+	return t
+>>>>>>> 0c220ce949a4e1329514b87575288e71b6381611
+}
